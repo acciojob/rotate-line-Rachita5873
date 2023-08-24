771ce23695acf25b5e2aafc7cbb54dc3dac06c87
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-08-24 08:52:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let line = document.getElementById('line');
+let angle = 0;
+setInterval(function() {
+  angle += 1;
+  line.style.transform = `rotate(${angle}deg)`
+}, 100);
